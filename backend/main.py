326adc60af803395bb65dc91b7e3eab29d8cb18d
jsbloom2020-beep <jsbloom2020@gmail.com>
2025-11-26 13:48:13 -0500

    Allow Render frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ CURRENT_DATE_OVERRIDE = dt.date.today()
 # --- CORS so React (localhost:3000) can talk to this API ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://workplanmvp.onrender.com",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
